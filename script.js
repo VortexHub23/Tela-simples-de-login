be2347467abb1d4ff94f0e39044118f574326c11
Otavio Lamas <tavinholamas123@gmail.com>
2026-04-08 21:15:53 -0300

    adiciona funçoes

--- script.js
+++ script.js
@@ -9,37 +9,63 @@ function clicar(event){
     
     lista.innerHTML =''
     let erros = []
-    if(email.value.length === 0 || senha.value.length === 0 || senha.value.length >=9){
+    if(email.value.length === 0 || senha.value.length === 0){
       email.style.border = '2px solid red'
-      email.placeholder = `incorreto`
+      email.placeholder = `Incorreto`
       senha.style.border = '2px solid red'
-      senha.placeholder = `incorreto`
-      erros.push('email ou senha incorretos')
+      senha.placeholder = `Incorreto`
+      erros.push('Email ou senha não possuem caracteres')
       let errop = document.createElement('li')
       errop.innerHTML = `${erros}`
       lista.appendChild(errop)
       email.value= ''
       email.focus()
       section.style.height = '340px'
-    
-    
-   
-   
       
-    } 
+    } else if(senha.value.length >= 9){
+     email.style.border = '2px solid red'
+      email.placeholder = `Incorreto`
+      senha.style.border = '2px solid red'
+      senha.placeholder = `Incorreto`
+      erros.push('Senha muito grande')
+      let errop = document.createElement('li')
+      errop.innerHTML = `${erros}`
+      lista.appendChild(errop)
+      email.value= ''
+      email.focus()
+      section.style.height = '340px'
+
+
+    }
+
+
+
+    }
+
+
+
+    
+
+    
+
+    
+
 function sair(){
     const email = document.getElementById('email')
     const senha = document.getElementById('password')
     const lista = document.getElementById('lista')
     const section = document.getElementById('section')
+    lista.innerHTML =''
+    let erros = []
       email.style.border = '2px solid black'
       email.placeholder = `Email...`
       senha.style.border = '2px solid black'
       senha.placeholder = `Senha...`
+      errop.innerHTML = ''
 
 
 
 }
 
 
-}
+
